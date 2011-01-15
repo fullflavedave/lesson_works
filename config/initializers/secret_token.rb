dab3515a45ffdf6fd0bac49a832f28e364c1b5b7
Dave Allen <dave@fullflavedave.com>
2011-01-14 18:09:11 -0800

    Initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessonWorks::Application.config.secret_token = 'a8f92d5386bb4a94f4ff3278a4a2a5db5a2b1c2aefece97c427e7de583c3dfc094d11ddb4b1d796c26af4adf7bcc659de93df16342947bc00fa960212b50c960'
